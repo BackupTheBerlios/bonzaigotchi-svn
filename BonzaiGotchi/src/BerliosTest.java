5f2624a815787898f491bc082bed3013c36abd45
dimjon <dimjon@76cfdae6-2025-0410-b47c-847d66e48861>
2009-11-16 12:34:47 +0000

    BerliosTest

--- BonzaiGotchi/src/BerliosTest.java
+++ BonzaiGotchi/src/BerliosTest.java
@@ -2,7 +2,7 @@
 /**
  * @author dimjon
  * 
- *
+ * Dies ist eine Klasse, die den Einstatz von SVN bei BerliOS demonstrieren soll...  :o)
  *
  */
 
---
+++ BonzaiGotchi/src/BerliosTest2.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author dimjon
+ * 
+ * Dies ist eine zweite Klasse, die den Einstatz von SVN bei BerliOS demonstrieren soll...
+ *
+ */
+
+public class BerliosTest2 {
+
+}
